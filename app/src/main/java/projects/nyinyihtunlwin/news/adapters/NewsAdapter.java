4fa66e648d1765e560bcc460c95f77b111dbb487
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2017-11-30 11:24:08 +0630

    Scroll State Modified!

--- app/src/main/java/projects/nyinyihtunlwin/news/adapters/NewsAdapter.java
+++ app/src/main/java/projects/nyinyihtunlwin/news/adapters/NewsAdapter.java
@@ -39,6 +39,6 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsViewHolder> {
 
     @Override
     public int getItemCount() {
-        return 16;
+        return 1;
     }
 }
--- app/src/main/java/projects/nyinyihtunlwin/news/components/SmartScrollListener.java
+++ app/src/main/java/projects/nyinyihtunlwin/news/components/SmartScrollListener.java
@@ -2,6 +2,7 @@ package projects.nyinyihtunlwin.news.components;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 /**
  * Created by Dell on 11/26/2017.
@@ -13,8 +14,9 @@ public class SmartScrollListener extends RecyclerView.OnScrollListener {
         void onListEndReached();
     }
 
-    private int visibleItemCount, pastVisibleItems, totalItemCount;
+    private int visibleItemCount, pastVisibleItems, totalItemCount, lastCompletelyVisibleItem;
     private boolean isListEndReached = false;
+    private boolean reachedOnce = false;
     private int previousDy, currentDy;
 
     private OnSmartScrollListener mSmartScrollListener;
@@ -26,7 +28,6 @@ public class SmartScrollListener extends RecyclerView.OnScrollListener {
     @Override
     public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
         super.onScrolled(recyclerView, dx, dy);
-
         currentDy = dy;
         if (currentDy > previousDy) {
             //from top to bottom
@@ -36,11 +37,15 @@ public class SmartScrollListener extends RecyclerView.OnScrollListener {
         }
 
         //get currently visible items count
+
+
         visibleItemCount = recyclerView.getChildCount();
 
         totalItemCount = recyclerView.getLayoutManager().getItemCount();
 
         pastVisibleItems = ((LinearLayoutManager) recyclerView.getLayoutManager()).findFirstVisibleItemPosition();
+
+        lastCompletelyVisibleItem = ((LinearLayoutManager) recyclerView.getLayoutManager()).findLastVisibleItemPosition();
 
         previousDy = currentDy;
     }
@@ -49,9 +54,24 @@ public class SmartScrollListener extends RecyclerView.OnScrollListener {
     public void onScrollStateChanged(RecyclerView recyclerView, int scrollState) {
         super.onScrollStateChanged(recyclerView, scrollState);
         if (scrollState == RecyclerView.SCROLL_STATE_IDLE) {
-            if ((visibleItemCount + pastVisibleItems) >= totalItemCount && !isListEndReached) {
-                isListEndReached = true;
-                mSmartScrollListener.onListEndReached();
+            if (totalItemCount >= 2) {
+                if ((visibleItemCount + pastVisibleItems) >= totalItemCount && !isListEndReached && !reachedOnce) {
+                    isListEndReached = true;
+                    reachedOnce = true;
+                    mSmartScrollListener.onListEndReached();
+                } else if ((lastCompletelyVisibleItem == (totalItemCount - 2)) && reachedOnce) {
+                    isListEndReached = false;
+                    reachedOnce = false;
+                }
+            } else {
+                /**
+                 * if total item count == 1
+                 */
+                if ((visibleItemCount + pastVisibleItems) >= totalItemCount && !isListEndReached && !reachedOnce) {
+                    isListEndReached = true;
+                    reachedOnce = true;
+                    mSmartScrollListener.onListEndReached();
+                }
             }
         }
     }
